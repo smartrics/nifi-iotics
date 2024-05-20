dbd880a1f6619ca4d3ddc25f7fa62e22671c30c0
Fabrizio Cannizzo <smartrics@gmail.com>
2024-05-20 23:00:33 +0100

    added unhappy paths and test

--- nifi-iotics-processors/src/main/java/smartrics/iotics/nifi/processors/tools/JsonToProperty.java
+++ nifi-iotics-processors/src/main/java/smartrics/iotics/nifi/processors/tools/JsonToProperty.java
@@ -7,26 +7,32 @@ public class JsonToProperty {
 
     public static Property fromJson(JsonObject o) {
         Property.Builder b = Property.newBuilder();
+        if(!o.has("key")) {
+            throw new IllegalArgumentException("Invalid property: missing \"key\"");
+        }
         b.setKey(o.get("key").getAsString());
         if (o.has("literal")) {
             Literal.Builder lb = Literal.newBuilder().setValue(o.get("literal").getAsString());
             if (o.has("dataType")) {
                 lb.setDataType(o.get("dataType").getAsString());
+            } else {
+                throw new IllegalArgumentException("Invalid property: literal without \"dataType\"");
             }
             b.setLiteralValue(lb.build());
-        }
-        if (o.has("")) {
+        } else if (o.has("langLiteral")) {
             LangLiteral.Builder lb = LangLiteral.newBuilder().setValue(o.get("langLiteral").getAsString());
             if (o.has("lang")) {
                 lb.setLang(o.get("lang").getAsString());
+            } else {
+                throw new IllegalArgumentException("Invalid property: langLiteral without \"lang\"");
             }
             b.setLangLiteralValue(lb.build());
-        }
-        if (o.has("stringLiteral")) {
+        } else if (o.has("stringLiteral")) {
             b.setStringLiteralValue(StringLiteral.newBuilder().setValue(o.get("stringLiteral").getAsString()).build());
-        }
-        if (o.has("uri")) {
+        } else if (o.has("uri")) {
             b.setUriValue(Uri.newBuilder().setValue(o.get("uri").getAsString()).build());
+        } else {
+            throw new IllegalArgumentException("Invalid property: missing one of \"uri\", \"literal\", \"stringLiteral\", \"langLiteral\" ");
         }
         return b.build();
     }
---
+++ nifi-iotics-processors/src/test/java/smartrics/iotics/nifi/processors/tools/JsonToPropertyTest.java
@@ -0,0 +1,108 @@
+package smartrics.iotics.nifi.processors.tools;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static smartrics.iotics.nifi.processors.tools.JsonToProperty.*;
+
+import com.google.gson.JsonObject;
+import com.iotics.api.Property;
+import org.junit.jupiter.api.Test;
+
+
+class JsonToPropertyTest {
+    @Test
+    public void testFromJsonMissingKey() {
+        JsonObject o = new JsonObject();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            fromJson(o);
+        });
+        assertThat(exception.getMessage(), is("Invalid property: missing \"key\""));
+    }
+
+    @Test
+    public void testFromJsonMissingLiteralDataType() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        o.addProperty("literal", "someLiteral");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            fromJson(o);
+        });
+        assertThat(exception.getMessage(), is("Invalid property: literal without \"dataType\""));
+    }
+
+    @Test
+    public void testFromJsonMissingLangLiteralLang() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        o.addProperty("langLiteral", "someLangLiteral");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            fromJson(o);
+        });
+        assertThat(exception.getMessage(), is("Invalid property: langLiteral without \"lang\""));
+    }
+
+    @Test
+    public void testFromJsonMissingAllOptionalFields() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            fromJson(o);
+        });
+        assertThat(exception.getMessage(), is("Invalid property: missing one of \"uri\", \"literal\", \"stringLiteral\", \"langLiteral\" "));
+    }
+
+    @Test
+    public void testFromJsonWithLiteral() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        o.addProperty("literal", "someLiteral");
+        o.addProperty("dataType", "someDataType");
+
+        Property property = fromJson(o);
+
+        assertThat(property.getKey(), is("someKey"));
+        assertThat(property.getLiteralValue().getValue(), is("someLiteral"));
+        assertThat(property.getLiteralValue().getDataType(), is("someDataType"));
+    }
+
+    @Test
+    public void testFromJsonWithLangLiteral() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        o.addProperty("langLiteral", "someLangLiteral");
+        o.addProperty("lang", "en");
+
+        Property property = fromJson(o);
+
+        assertThat(property.getKey(), is("someKey"));
+        assertThat(property.getLangLiteralValue().getValue(), is("someLangLiteral"));
+        assertThat(property.getLangLiteralValue().getLang(), is("en"));
+    }
+
+    @Test
+    public void testFromJsonWithStringLiteral() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        o.addProperty("stringLiteral", "someStringLiteral");
+
+        Property property = fromJson(o);
+
+        assertThat(property.getKey(), is("someKey"));
+        assertThat(property.getStringLiteralValue().getValue(), is("someStringLiteral"));
+    }
+
+    @Test
+    public void testFromJsonWithUri() {
+        JsonObject o = new JsonObject();
+        o.addProperty("key", "someKey");
+        o.addProperty("uri", "http://example.com");
+
+        Property property = fromJson(o);
+
+        assertThat(property.getKey(), is("someKey"));
+        assertThat(property.getUriValue().getValue(), is("http://example.com"));
+    }
+
+}
