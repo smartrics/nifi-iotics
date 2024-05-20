34ef45f7a6d7d4794cc2d804ed3909531d2b1722
Fabrizio Cannizzo <smartrics@gmail.com>
2024-05-20 23:00:03 +0100

    fixed finder test

--- nifi-iotics-processors/src/test/java/smartrics/iotics/nifi/processors/IoticsFinderIT.java
+++ nifi-iotics-processors/src/test/java/smartrics/iotics/nifi/processors/IoticsFinderIT.java
@@ -1,5 +1,7 @@
 package smartrics.iotics.nifi.processors;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.nifi.util.MockFlowFile;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
@@ -8,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static smartrics.iotics.nifi.processors.IoticsControllerServiceFactory.injectIoticsHostService;
 
 public class IoticsFinderIT {
@@ -18,10 +21,14 @@ public class IoticsFinderIT {
         testRunner = TestRunners.newTestRunner(IoticsFinder.class);
         injectIoticsHostService(testRunner);
 
-        testRunner.setProperty(IoticsFinder.EXPIRY_TIMEOUT, "10");
+        testRunner.setProperty(IoticsFinder.EXPIRY_TIMEOUT, "3");
 //        testRunner.setProperty(IoticsFinder.LOCATION, "{ 'r': 5, 'lat': 52.568213, 'lon': -0.244837 }");
-        testRunner.setProperty(IoticsFinder.TEXT, "Toyota");
-//        testRunner.setProperty(IoticsFinder.PROPERTIES);
+//        testRunner.setProperty(IoticsFinder.TEXT, "Toyota");
+        testRunner.setProperty(IoticsFinder.PROPERTIES, """
+[
+{"key": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "uri":"http://schema.org/Car"}
+]
+""");
         testRunner.setProperty(Constants.QUERY_SCOPE, "LOCAL");
     }
 
@@ -35,11 +42,13 @@ public class IoticsFinderIT {
 
         if (!results.isEmpty()) {
             results.forEach(mockFlowFile -> {
-                String outputFlowfileContent = new String(testRunner.getContentAsByteArray(mockFlowFile));
-                System.out.println(outputFlowfileContent);
+                String json = new String(testRunner.getContentAsByteArray(mockFlowFile));
+                JsonObject root = JsonParser.parseString(json).getAsJsonObject();
+                assertTrue(root.has("id"));
+                assertTrue(root.has("hostDid"));
             });
         } else {
-            System.out.println("NOTHING BACK");
+            throw new RuntimeException("Nothing back!");
         }
     }
 
